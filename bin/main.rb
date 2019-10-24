db6939ee8746a1bb6958bc184b52a08cf28c30ff
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-10-24 19:33:23 +0300

    Added Prompts for User input - Addded Empty grid

--- bin/main.rb
+++ bin/main.rb
@@ -2,4 +2,34 @@
 
 # frozen_string_literal: true
 
-puts 'Hello World!'
+puts "***************"
+puts "**TIC TAC TOE**"
+puts "***************"
+
+p "Player X Enter name: "
+player_x = gets.chomp
+
+p "Player O Enter name: "
+player_O = gets.chomp
+
+puts "#{player_x} is playing X and #{player_O} is playing O"
+
+play_first  = rand(0..1)
+
+play_first = play_first === 0 ? player_O : player_x
+
+puts "\n******************"
+puts "Player #{play_first} will play first"
+
+#TODO
+#Instructions for the game line 8
+# PLay empty grid
+
+grid = [["*", "*", "*"], ["*", "*", "*"], ["*", "*", "*"]]
+
+grid.each do |row|
+    row.each do |col|
+        print "#{col}\t "
+    end
+    puts " \n\n\n"
+end
