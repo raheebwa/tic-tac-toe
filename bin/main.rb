6935a51b9f891173185ad80563d1117ad38cb82b
Marcos Medeiros <mvsmdrs@protonmail.com>
2019-10-25 17:26:40 -0300

    Load player class inside game.rb

--- bin/main.rb
+++ bin/main.rb
@@ -2,4 +2,58 @@
 
 # frozen_string_literal: true
 
-puts 'Hello World!'
+load "lib/game.rb"
+
+
+puts "***************"
+puts "**TIC TAC TOE**"
+puts "***************"
+
+print "\n\n\n"
+
+p "You and your friend should input your names to initialize the game"
+
+print "\n\n\n"
+
+print "Player 1 Enter name:"
+player_1 = gets.chomp
+
+print "Player 2 Enter name:"
+player_2 = gets.chomp
+
+print "\n\n\n"
+
+game = Game.new(player_1, player_2)
+game.display
+
+print "#{player_1} is playing X and #{player_2} is playing O \n\n\n"
+
+puts "\n******************\n\n\n"
+
+while game.status == false
+  p "#{game.curr_player}, it's your turn! Input your choice."
+  prompt_move = gets.chomp
+  game.make_move(prompt_move)
+
+
+end
+    
+# display prompt to move input to player 1
+p "input your move"
+prompt_move = gets.chomp
+
+
+# execute function to display grid with players mark
+
+# display prompt to move input to player 2
+
+# execute function to display grid with players mark
+
+# repeat the above 
+
+if game_won || game_ended || game_drawn
+    # display winner & prompt for replay
+    game_status = false
+    
+end
+
--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 load 'lib/grid.rb'
+load 'lib/player.rb'
 
 class Game
-  attr_accessor :winner
-
   @@current_player = true
 
+  @@finished = false
+
   def initialize(player_1, player_2, finished = false)
-    @player_1 = player_1
-    @player_2 = player_2
+    @player_1 = Player.new(player_1)
+    @player_2 = Player.new(player_2)
     @finished = finished
     @@played_moves = {}
     @grid = Grid.new
-
   end
 
   def display
@@ -22,66 +22,75 @@ class Game
 
   def validate_move(move)
     return false unless (1..9).member?(move) && !@@played_moves.member?(move)
+
     true
   end
 
   def make_move(move)
-    mark = @@current_player ? "X" : "O"
+    mark = @@current_player ? 'X' : 'O'
     if validate_move(move)
       @grid.translate(move, mark)
       display
       switch_player
       @@played_moves[move] = mark
-      puts "Moved played #{@@played_moves.count}"
       true
     else
-      puts "Your move is invalid"
       false
     end
+  end
+
+  def status
+    return @@finished
+  end
+
+  def curr_player
+    return @@current_player == true ? @player_1.name : @player_2.name
   end
 
   def switch_player
     @@current_player = !@@current_player
   end
 
-  def moves_played
-   return  @@played_moves
-  end
-
   def check_win
-    if @@played_moves.count > 4
-      x_count = @@played_moves.select{|x,y| y=="X"}
-      o_count = @@played_moves.select{|a,b| b=="O"}
+    return false unless @@played_moves.count > 4
 
-      if (x_count.count > 2)
-        valid_win(x_count)
-        elsif(o_count.count > 2)        
-        valid_win(o_count)
-      end
+    x_count = @@played_moves.select { |_x, y| y == 'X' }
+    o_count = @@played_moves.select { |_a, b| b == 'O' }
+
+    if x_count.count > 2
+      @@finished = true if valid_win(x_count)      
+      
+    elsif o_count.count > 2
+      @@finished = true if valid_win(o_count)
+      
+    else
+      false
     end
-    false
   end
 
   def valid_win(hash)
-
     if (hash[1] == hash[2]) == hash[3]
-      return true
+      true
     elsif (hash[4] == hash[5]) == hash[6]
-      return true
+      true
     elsif (hash[7] == hash[8]) == hash[9]
-      return true
+      true
     elsif (hash[1] == hash[4]) == hash[7]
-      return true
+      true
     elsif (hash[2] == hash[5]) == hash[8]
-      return true
+      true
     elsif (hash[3] == hash[6]) == hash[9]
-      return true
+      true
     elsif (hash[7] == hash[5]) == hash[3]
-      return true
-    elsif (hash[1] == hash[5]) == hash[9]
-      return true
+      true
     else
-      return false
+      (hash[1] == hash[5]) == hash[9]
     end
+  end
+
+  def instructions
+    print "- Each player should make one move per turn.\n\n\n"
+    print "- To make a move, input the number of an empty cell that you wish to mark.\n\n\n"
+    print "- The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner \n\n\n"
   end
 end
